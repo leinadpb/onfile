556228f13190ed7ffdd79e3b35073e9be2eff278
Daniel Pena <digrape07@gmail.com>
2018-06-18 20:53:36 -0400

    Gulp and sass added.

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+﻿var gulp = require("gulp"),
+    fs = require("fs"),
+    sass = require("gulp-sass");
+
+gulp.task("site-sass", function () {
+    return gulp.src('Styles/site.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('wwwroot/css'));
+});
